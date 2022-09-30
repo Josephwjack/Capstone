cc210f04d539d7aef1a9aedab2d39baab95c0b14
Joseph Jack <joseph.w.jack@gmail.com>
2022-09-30 08:48:02 -0700

    first two api calls working

--- src/components/Control.js
+++ src/components/Control.js
@@ -45,6 +45,45 @@ function GetLocation() {
       // });
   }, [])
 
+  useEffect(() => {
+    console.log(`https://maps.googleapis.com/maps/api/geocode/json?address=new+york&key=${process.env.REACT_APP_API_KEY}
+    `)
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=new+york&key=${process.env.REACT_APP_API_KEY}
+    `)
+      .then((response) => {
+        if (!response.ok) {
+          console.log("response");
+          throw new Error(`${response.status}: ${response.statusText}`);
+        } else {
+          return response.json()
+        }
+      })
+      .then((jsonifiedResponse) => {
+        const latLng2 = (jsonifiedResponse.results[0].geometry.location);
+        console.log(latLng2);
+        setSecondLocation(latLng2)
+          
+        })
+        setIsLoaded(true);   
+  }, [])
+
+  setResult(firstLocation + secondLocation) / 2;
+  console.log(showResult);
+  const handleFindingResultCoordinates = (firstLocation, secondLocation) => {
+    let lat3 = (firstLocation.lat + secondLocation.lat) / 2
+    let lng3 = (firstLocation.lng + secondLocation.lng) / 2
+    setResult(lat3, lng3)
+    console.log(lat3, lng3);
+    setIsLoaded(true);
+    handleFindingResultCoordinates();
+  }
+
+  const formSubmissionHandler = (e, location1, location2) => {
+    e.preventDefault();
+    handleFindingResultCoordinates();
+    setFormVisibleOnPage(!formVisibleOnPage);
+    return false;
+  }
   
   
   let currentlyVisibleState = null;
